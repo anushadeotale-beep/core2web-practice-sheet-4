015ca4a24cb2e45f93af3dcede6d678ecd1e1542
Anusha Deotale <anushadeotale@gmail.com>
2026-07-29 21:26:11 +0530

    Add files via upload

---
+++ question6.c
@@ -0,0 +1,13 @@
+//write a program to print the lowercase alphabets in reverse order
+#include<stdio.h>
+
+
+int main()
+{
+ char alphabets ;
+ for(int i=122;i>=97;i--)
+ {
+    printf("%c \n",i);
+ }
+ return 0;
+}
