9f727f987d2a2a47ffcc9a00aa95786def861eaf
Anusha Deotale <anushadeotale@gmail.com>
2026-07-29 18:40:35 +0530

    Add files via upload

---
+++ question1.c
@@ -0,0 +1,18 @@
+//write a program too print first 100 natural numbers
+
+#include<stdio.h>
+int main()
+{
+    int i;
+
+    int num=101;
+  
+    
+    for(i=0;i<num;i++)
+    {
+       
+   printf("%d \n",i);
+
+    }
+return 0;
+}
